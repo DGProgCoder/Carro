57bc83b37549871ee784350e6f71ac967b6ed386
DGProgCoder <dgcantor09@gmail.com>
2025-04-30 15:33:51 -0300

    Add files via upload

---
+++ carro.py
@@ -0,0 +1,12 @@
+Cliente1 = "Jorginho"
+Cliente2 = "Juninho"
+
+Carro1 = "F250"
+Carro2 = "Silverado"
+
+Marca1 = "Ford"
+Marca2 = "Chevrolet"
+
+Valor1 = "179.999,00"
+Valor2 = "124.999,00"
+print(f"{Cliente1} está em busca de uma {Marca1} {Carro1}."f"\n\n{Cliente2} está em busca de uma {Marca2} {Carro2}."F"\n\nTemos uma {Carro1} por {Valor1} e uma {Carro2} por {Valor2}.")
